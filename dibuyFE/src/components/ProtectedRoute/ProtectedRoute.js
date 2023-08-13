5808f99dbee7451cb315b7264650cad817a6654d
Prakash Dumpa <prakashdumpa143@gmail.com>
2023-08-13 12:31:10 +0530

    cart Handled

--- dibuyFE/src/components/ProtectedRoute/ProtectedRoute.js
+++ dibuyFE/src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,24 +1,25 @@
-import Cookies from 'js-cookie'
-import { Redirect, Route } from 'react-router-dom'
-import DiBuyContext from '../../context/DiBuyContext'
+import Cookies from "js-cookie";
+import { Redirect, Route } from "react-router-dom";
+import DiBuyContext from "../../context/DiBuyContext";
 const ProtectedRoute = (props) => {
-    if (Cookies.get("jwtToken") === undefined) {
-        return <Redirect to="/user/login" />
-    }
+  if (Cookies.get("jwtToken") === undefined) {
+    return <Redirect to="/user/login" />;
+  }
 
+  return (
+    <>
+      <DiBuyContext.Consumer>
+        {(value) => {
+          const { currentRoute, setCurrentRoute } = value;
+          //   console.log("currentRoute", currentRoute);
+          if (props.path !== currentRoute) {
+            setCurrentRoute(props.path);
+          }
+          return <Route {...props} />;
+        }}
+      </DiBuyContext.Consumer>
+    </>
+  );
+};
 
-    return (
-        <DiBuyContext.Consumer>
-            {value => {
-                const { currentRoute, setCurrentRoute } = value
-                if (props.path !== currentRoute) {
-                    setCurrentRoute(props.path)
-                }
-                return <Route {...props} />
-            }}
-        </DiBuyContext.Consumer>
-    )
-
-}
-
-export default ProtectedRoute
+export default ProtectedRoute;
